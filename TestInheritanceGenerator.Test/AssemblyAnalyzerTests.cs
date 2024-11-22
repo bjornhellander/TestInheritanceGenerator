84adc0b89e2f3a707d86c37a70d061f640f054b3
Björn Hellander <bjorn.hellander@gmail.com>
2024-11-22 18:00:21 +0100

    Added an analyzer that reports diagnostics in the same situations that cause the generator to not generate any code

---
+++ TestInheritanceGenerator.Test/AssemblyAnalyzerTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+using VerifyCS = TestInheritanceGenerator.Test.Verifiers.CSharpAnalyzerVerifier<
+    TestInheritanceGenerator.AssemblyAnalyzer>;
+
+namespace TestInheritanceGenerator.Test
+{
+    [TestClass]
+    public class AssemblyAnalyzerTests
+    {
+        [TestMethod]
+        public async Task TestNoTests()
+        {
+            var test = new VerifyCS.Test()
+            {
+                TestState =
+                {
+                    Sources = { "" },
+                },
+                ExpectedDiagnostics = { VerifyCS.Diagnostic(AssemblyAnalyzer.IncorrectAssemblyNameDiagnosticId) },
+            };
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        [DataRow("1")]
+        [DataRow("1_1")]
+        [DataRow("1_1_1")]
+        [DataRow("1_1_1_1")]
+        public async Task TestWithoutBaseAssembly(string version)
+        {
+            var test = new VerifyCS.Test()
+            {
+                TestState =
+                {
+                    Sources = { "" },
+                },
+                ExpectedDiagnostics = { VerifyCS.Diagnostic(AssemblyAnalyzer.NoBaseAssemblyDiagnosticId) },
+            };
+
+            test.SolutionTransforms.Add((solution, projectId) =>
+            {
+                var project = solution.GetProject(projectId);
+                if (project.Name == "TestProject")
+                {
+                    solution = solution.WithProjectAssemblyName(projectId, "Example.Tests" + version);
+                }
+
+                return solution;
+            });
+
+            await test.RunAsync();
+        }
+
+        [TestMethod]
+        [DataRow("1", "2")]
+        [DataRow("1_1", "1_2")]
+        [DataRow("1_1_1", "1_1_2")]
+        [DataRow("1_1_1_1", "1_1_1_2")]
+        public async Task TestWithBaseAssembly(string version1, string version2)
+        {
+            var test = new VerifyCS.Test()
+            {
+                TestState =
+                {
+                    Sources = { "" },
+                    AdditionalProjectReferences =
+                    {
+                        "Example.Tests" + version1,
+                    },
+                    AdditionalProjects =
+                    {
+                        ["Example.Tests" + version1] =
+                        {
+                            Sources =
+                            {
+                                ("Tests.cs", ""),
+                            },
+                        },
+                    },
+                },
+                ExpectedDiagnostics = {
+                    VerifyCS.Diagnostic(AssemblyAnalyzer.NoBaseTestsDiagnosticId), // From the main project
+                    VerifyCS.Diagnostic(AssemblyAnalyzer.NoBaseAssemblyDiagnosticId), // From the additional project
+                },
+            };
+
+            test.SolutionTransforms.Add((solution, projectId) =>
+            {
+                var project = solution.GetProject(projectId);
+                if (project.Name == "TestProject")
+                {
+                    solution = solution.WithProjectAssemblyName(projectId, "Example.Tests" + version2);
+                }
+                return solution;
+            });
+
+            await test.RunAsync();
+        }
+    }
+}
---
+++ TestInheritanceGenerator/AssemblyAnalyzer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+// TODO: Check these suppressions
+#pragma warning disable RS1004 // Recommend adding language support to diagnostic analyzer
+#pragma warning disable RS2008 // Enable analyzer release tracking
+
+namespace TestInheritanceGenerator
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AssemblyAnalyzer : DiagnosticAnalyzer
+    {
+        public const string IncorrectAssemblyNameDiagnosticId = "TestInh001";
+        public const string NoBaseAssemblyDiagnosticId = "TestInh002";
+        public const string NoBaseTestsDiagnosticId = "TestInh003";
+
+        private static readonly DiagnosticDescriptor IncorrectAssemblyNameDescriptor = new(
+            IncorrectAssemblyNameDiagnosticId,
+            "Use consistent line endings",
+            "Use consistent line endings",
+            "SourceGenerator",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Use consistent line endings.",
+            customTags: WellKnownDiagnosticTags.CompilationEnd);
+
+        private static readonly DiagnosticDescriptor NoBaseAssemblyDescriptor = new(
+            NoBaseAssemblyDiagnosticId,
+            "Use consistent line endings",
+            "Use consistent line endings",
+            "SourceGenerator",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Use consistent line endings.",
+            customTags: WellKnownDiagnosticTags.CompilationEnd);
+
+        private static readonly DiagnosticDescriptor NoBaseTestsDescriptor = new(
+            NoBaseTestsDiagnosticId,
+            "Use consistent line endings",
+            "Use consistent line endings",
+            "SourceGenerator",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "Use consistent line endings.",
+            customTags: WellKnownDiagnosticTags.CompilationEnd);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(
+                IncorrectAssemblyNameDescriptor,
+                NoBaseAssemblyDescriptor,
+                NoBaseTestsDescriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationAction(HandleCompilation);
+        }
+
+        private void HandleCompilation(CompilationAnalysisContext context)
+        {
+            var compilation = context.Compilation;
+            if (!Helpers.TryGetAssemblyNamePrefixAndVersion(compilation.AssemblyName, out var assemblyPrefix, out _))
+            {
+                ReportDiagnostic(context, IncorrectAssemblyNameDescriptor);
+                return;
+            }
+
+            if (!Helpers.TryGetBaseAssembly(compilation, assemblyPrefix, out var baseAssembly, out _))
+            {
+                ReportDiagnostic(context, NoBaseAssemblyDescriptor);
+                return;
+            }
+
+            var types = Helpers.GetTestTypes(baseAssembly);
+            if (types.Length == 0)
+            {
+                ReportDiagnostic(context, NoBaseTestsDescriptor);
+                return;
+            }
+        }
+
+        private static void ReportDiagnostic(CompilationAnalysisContext context, DiagnosticDescriptor descriptor)
+        {
+            var diagnostic = Diagnostic.Create(descriptor, null);
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
